62ad6ef4f8667251422a353f5b587caad7350d57
James Aules <james_aules@icloud.com>
2023-06-29 18:28:03 -0400

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import {Navbar, Comtainer} from ("react-bootstrap")
+
+const NavBar = () => {
+    return(
+        <NavBar>
+            <Comtainer>
+                <NavBar.Brand></NavBar.Brand>
+            </Comtainer>
+        </NavBar>
+    )
+}
+
+export default NavBar;
