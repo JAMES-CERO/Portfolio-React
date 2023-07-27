a8c9cc3ba037a0cb675ae3dcc53238a2bc4589e8
James Aules <james_aules@icloud.com>
2023-07-27 15:02:04 -0400

    Hotfiox

--- src/components/MailChimpForm.jsx
+++ src/components/MailChimpForm.jsx
@@ -5,17 +5,16 @@ import NewsLetter from "./NewsLetter";
 const MailChimpForm = () => {
     const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`
 
-    return(
+    return (
         <>
-            <MailchimpSubscribe url={postUrl} render={({subscribe, status, message}) => (
+            <MailchimpSubscribe url={postUrl} render={({ subscribe, status, message }) => (
                 <NewsLetter
                     status={status}
                     message={message}
                     onValidated={formData => subscribe(formData)}
                 />
-            )}>
-
-            </MailchimpSubscribe>
+            )}
+            />
         </>
     )
 }
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -71,7 +71,7 @@ const Project = () => {
                             }
                           </Row>
                         </Tab.Pane>
-                        <Tab.Pane eventKey="section">
+                        <Tab.Pane eventKey="second">
                           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
                         </Tab.Pane>
                         <Tab.Pane eventKey="third">
